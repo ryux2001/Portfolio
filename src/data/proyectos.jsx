1b66043400483c8be0b03ba1131800835758b9a4
Rynaldo Bux <rynaldobuxeng@gmail.com>
2026-02-24 20:51:07 +0100

    Fix: imagenes

--- src/data/proyectos.jsx
+++ src/data/proyectos.jsx
@@ -2,20 +2,20 @@ export const listaProyectos = [
   {
     id: 1,
     titulo: "Components PC (En desarrollo)",
-    imagen: "/public/ImagenesProyectos/ComponentsPC/Components-pc.png",
+    imagen: "/ImagenesProyectos/ComponentsPC/Components-pc.png",
     descripcion:
       "App Web con catálogo de componentes de PC (gráficas y procesadores). Compara precios/especificaciones y coloca una nota de calidad/precio ",
     tecnologias: ["JavaScript", "React", "Tailwind", "Supabase"],
     url: "https://componetsviewer.vercel.app/",
     galeria: [
-      "/public/ImagenesProyectos/ComponentsPC/Imagen-1.png",
-      "/public/ImagenesProyectos/ComponentsPC/Imagen-2.png",
+      "/ImagenesProyectos/ComponentsPC/Imagen-1.png",
+      "/ImagenesProyectos/ComponentsPC/Imagen-2.png",
     ],
   },
   {
     id: 2,
     titulo: "Music Quiz",
-    imagen: "/public/ImagenesProyectos/MusicQuiz/MusicQuiz.png",
+    imagen: "/ImagenesProyectos/MusicQuiz/MusicQuiz.png",
     descripcion:
       "Juego de preguntas y respuestas sobre música, con diferentes categorías y niveles de dificultad, con sistema de comodines y puntuación global ",
     tecnologias: ["Javascript", "React", "Supabase"],
