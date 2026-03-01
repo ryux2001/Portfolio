e3e64d19eb45972bef5e65aa98361179e4f7d7f5
Rynaldo Bux <rynaldobuxeng@gmail.com>
2026-03-01 17:19:27 +0100

    Agregado FDrive

--- src/data/proyectos.jsx
+++ src/data/proyectos.jsx
@@ -5,7 +5,7 @@ export const listaProyectos = [
     imagen: "/ImagenesProyectos/ComponentsPC/Components-pc.png",
     descripcion:
       "App Web con catálogo de componentes de PC (gráficas y procesadores). Compara precios/especificaciones y coloca una nota de calidad/precio ",
-    tecnologias: ["JavaScript", "React", "Tailwind", "Supabase"],
+    tecnologias: ["JavaScript", "React", "Tailwindcss", "Supabase"],
     url: "https://componetsviewer.vercel.app/",
     galeria: [
       "/ImagenesProyectos/ComponentsPC/Imagen-1.png",
@@ -23,6 +23,22 @@ export const listaProyectos = [
     galeria: [
       "/ImagenesProyectos/MusicQuiz/Imagen-1.png",
       "/ImagenesProyectos/MusicQuiz/Imagen-2.png",
+    ],
+  },
+  {
+    id: 3,
+    titulo: "FDrive",
+    imagen: "/ImagenesProyectos/FDrive/Imagen-1.png",
+    descripcion:
+      "Aplicación de gestión de gastos enfocada a la paquetería; la hice para mí porque es el trabajo de un familiar y ayudo con la gestión. Esta aplicación cubre sus necesidades.",
+    tecnologias: ["Javascript", "React", "Tailwindcss", "Supabase"],
+    url: "https://f-drive.vercel.app/", // TU ENLACE AQUÍ
+    galeria: [
+      "/ImagenesProyectos/FDrive/Imagen-1.png",
+      "/ImagenesProyectos/FDrive/Imagen-2.png",
+      "/ImagenesProyectos/FDrive/Imagen-3.png",
+      "/ImagenesProyectos/FDrive/Imagen-4.png",
+      "/ImagenesProyectos/FDrive/Imagen-5.png",
     ],
   },
 ];
